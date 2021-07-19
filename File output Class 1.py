b7175a249bf44283e500316d05ab154c654437a1
Nafisa Ahmed <naffesaelger@gmail.com>
2021-07-19 22:57:55 +0530

    Class 4

---
+++ File output Class 1.py
@@ -0,0 +1,7 @@
+#class1.txt (input , function description, output) start with class 2
+#sample - reverse name
+'''
+Task - To reverse a name given by user input
+user input: "user input"
+reverse name: ...output
+'''
---
+++ file_read.py
@@ -0,0 +1,34 @@
+import os
+
+curr_working_dir = os.getcwd()
+
+#creating a file
+fp = open("temp_file.txt","w")
+temp_list = ["abc","def","ghi"]
+for each_elem in temp_list:
+    fp.write(each_elem + "\n")
+fp.close()
+
+#count num of lines in a file
+fp = open("temp_file.txt","r")
+
+file_content = fp.read() #read entire file and store in the variable
+file_content_list = file_content.split("\n")
+#num_lines = len(file_content_list)
+count =0
+for each_elem in file_content_list:
+    if each_elem:
+        count+=1
+num_lines = count
+fp.close()
+print(count)
+
+#for deleting file
+os.remove("filename here")
+
+#check if file exists:
+#os.path.exists(pathtofile) - if exists, it will return True
+
+#delete folder (directory)
+os.rmdir("folder name")
+
